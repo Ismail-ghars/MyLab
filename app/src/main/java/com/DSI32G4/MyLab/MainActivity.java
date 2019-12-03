3a0c71867dc3c9858b3e564e4d784c710be52182
AbdAzizFitouri <mohamedabdaziz.fitouri@kelibia.r-iset.tn>
2019-12-03 23:02:15 +0100

    Authentification Screen (BETA)

--- app/src/main/java/com/DSI32G4/MyLab/MainActivity.java
+++ app/src/main/java/com/DSI32G4/MyLab/MainActivity.java
@@ -4,13 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApi;
+
 public class MainActivity extends AppCompatActivity {
 
     //CONTRIBUTION TEST
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
     }
 }
