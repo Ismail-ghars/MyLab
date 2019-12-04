7332d83b21f96653dcf1ef8aa1dbff1b6b244ca6
AbdAzizFitouri <mohamedabdaziz.fitouri@kelibia.r-iset.tn>
2019-12-04 01:36:03 +0100

    Forgotten Password Screen (BETA) +French and English XML Strings Files (Bilingue)

--- app/src/main/java/com/DSI32G4/MyLab/MainActivity.java
+++ app/src/main/java/com/DSI32G4/MyLab/MainActivity.java
@@ -2,24 +2,37 @@ package com.DSI32G4.MyLab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
-    //CONTRIBUTION TEST
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 100;
+
+    SignInButton signInButton = findViewById(R.id.sign_in_button);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+
 
 
     @Override
     protected void onStart() {
         super.onStart();
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        //updateUI(account);
+        //getProfileInformation(account);
     }
 
     @Override
@@ -27,7 +40,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.sign_in_button:
+                        SignIn();
+                        break;
+                }
+            }
+        });
+
     }
+
+    private void SignIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void getProfileInformation(GoogleSignInAccount acct){
+        if(acct != null){
+            String firstName = acct.getDisplayName();
+            String familyName = acct.getFamilyName();
+            String email = acct.getEmail();
+
+            Bundle user = new Bundle();
+            user.putString("firstName",firstName);
+            user.putString("familyName",familyName);
+            user.putString("email",email);
+        }
+    }
+
+
 }
